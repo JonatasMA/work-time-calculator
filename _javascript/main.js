55197309fbe22ead9196a373e66268972c5c6740
Jonatas <jonatasmacedo70@hotmail.com>
2021-10-07 21:12:36 -0300

    fix: typos;
    feat: add dropdown for configuration

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,24 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('daily-hours').value = localStorage.getItem('daily-hours') || "08:00"
-  document.getElementById('notification').checked = localStorage.getItem('notification') == 'true' ? true : false
+  document.getElementById('daily-hours').value = fetchValue('daily-hours') || "08:00"
+  document.getElementById('start-1').value = fetchValue('start-1') || "08:00"
+  document.getElementById('stop-1').value = fetchValue('stop-1') || "11:00"
+  document.getElementById('start-2').value = fetchValue('start-2') || "12:00"
+  document.getElementById('notification').checked = fetchValue('notification') == 'true' ? true : false
   setHour();
 });
 
 var notification = document.getElementById('notification');
 var notificationInterval = 0;
 
-const start1 = document.getElementById('start-1');
-const start2 = document.getElementById('start-2');
-const stop1 = document.getElementById('stop-1');
-const stop2Elemnt = document.getElementById('stop-2');
+function toggleDropdown(elem) {
+  const backdrop = document.getElementById('backdrop');
+  console.log(elem)
+  if (elem.className.indexOf('is-active') >= 0) {
+    elem.className = 'dropdown margin-button';
+    backdrop.className = '';
+  } else {
+    elem.className += ' is-active';
+    backdrop.className = 'backdrop';
+  }
+}
+
+function storeValue(name, value) {
+  localStorage.setItem(name, value);
+}
+
+function fetchValue(name) {
+  return localStorage.getItem(name);
+}
 
 function setHour() {
+  const start1 = document.getElementById('start-1').value;
+  storeValue('start-1', start1);
+  const start2 = document.getElementById('start-2').value;
+  storeValue('start-2', start2);
+  const stop1 = document.getElementById('stop-1').value;
+  storeValue('stop-1', stop1);
   const daily = document.getElementById('daily-hours').value;
-
-  const morning = subtractTime(stop1.value, start1.value);
+  storeValue('daily-hours', daily);
+  
+  const stop2Elemnt = document.getElementById('stop-2');
+  const morning = subtractTime(stop1, start1);
   const leftTime = subtractTime(daily, morning);
 
-  stop2Elemnt.value = addTime(start2.value, leftTime);
+  stop2Elemnt.value = addTime(start2, leftTime);
 }
 
 function subtractTime(minundo, subtrahend) {
@@ -46,14 +72,18 @@ function turnOnNotifications() {
     clearInterval(notificationInterval);
     notificationInterval = setInterval(() => {
       if (notification.checked && (new Date).getSeconds() == 0) {
+        const stop1 = document.getElementById('stop-1').value;
+        const start2 = document.getElementById('start-2').value;
+        const stop2Elemnt = document.getElementById('stop-2').value;
         const actualy = normalizeTime(new Date());
         const icon = 'https://jonatasma.github.io/work-time-calculator/assets/img/android-chrome-512x512.png';
+        console.log(actualy == stop1, actualy == start2, actualy == stop2Elemnt);
         switch (actualy) {
-          case stop1.value:
+          case stop1:
             new Notification('Time to lunch', { icon: icon })
-          case start2.value:
+          case start2:
             new Notification('Oh no, go back to work!', { icon: icon })
-          case stop2Elemnt.value:
+          case stop2Elemnt:
             new Notification('All done, go rest!', { icon: icon })
         }
       }
