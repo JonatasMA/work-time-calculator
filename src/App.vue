7601e76cc68d17931154470449f4f58cb886d618
Jonatas Macedo <jonatasmacedo70@hotmail.com>
2023-07-15 13:46:07 -0300

    fix: fixed responsivenesson mobile

--- src/App.vue
+++ src/App.vue
@@ -15,108 +15,109 @@ import { toggleLanguage, dict } from "./assets/js/lang.js";
 import { storeValue, fetchValue } from "./assets/js/helpers";
 
 document.addEventListener('DOMContentLoaded', function () {
-  M.AutoInit();
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  M.Dropdown.init(elems, { closeOnClick: false, constrainWidth: false });
-  toggleLanguage();
+    M.AutoInit();
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, { closeOnClick: false, constrainWidth: false });
+    toggleLanguage();
 });
 
 function showLanguageMenu(lang) {
-  toggleLanguage(lang);
-  showLanguages();
+    toggleLanguage(lang);
+    showLanguages();
 }
 
 function showLanguages() {
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var dropDown = M.Dropdown.getInstance(elems[0]);
-  setTimeout(() => {
-    dropDown.recalculateDimensions();
-  }, 350);
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var dropDown = M.Dropdown.getInstance(elems[0]);
+    setTimeout(() => {
+        dropDown.recalculateDimensions();
+    }, 350);
 }
 
 var notifications = fetchValue('notification') === 'true';
 </script>
 
 <template>
-  <div class="card-content black-text">
-    <span id="title" class="card-title padding-bottom-16">How much time do I need to work today?</span>
-    <i class="material-icons right dropdown-trigger" data-target='dropdown1'>more_vert</i>
-    <InputBox />
-    <div class="col s12 box-gh-badge">
-      <GitHubBadge />
+    <div class="card-content black-text">
+        <span id="title" class="card-title padding-bottom-16">How much time do I need to work today?</span>
+        <i class="material-icons right dropdown-trigger" data-target='dropdown1'>more_vert</i>
+        <InputBox />
+        <div class="col s12 box-gh-badge">
+            <GitHubBadge />
+        </div>
     </div>
-  </div>
-  <ul id='dropdown1' class='dropdown-content collapsible dropdownWidth'>
-    <li>
-      <Switch id="notifications" label="Notifications" :disabled="false" v-model="notifications"/>
-    </li>
-    <!-- <li>
+    <ul id='dropdown1' class='dropdown-content collapsible dropdownWidth'>
+        <li>
+            <Switch id="notifications" label="Notifications" :disabled="false" v-model="notifications" />
+        </li>
+        <!-- <li>
       <Switch id="theme" label="Dark mode" :disabled="true" />
     </li> -->
-    <li class="divider" tabindex="-1"></li>
-    <li>
-      <div class="collapsible-header" @click="showLanguages"><i class="material-icons">language</i>
-        <p id="language">Language</p>
-      </div>
-      <div class="collapsible-body padding-1rem" style="padding: 1rem !important;">
-    <li v-for="(lang, index) in dict" :key="index">
-      <a href="#!" @click="showLanguageMenu(index)">{{ lang.name }}</a>
-    </li>
-    </div>
-    </li>
-  </ul>
+        <li class="divider" tabindex="-1"></li>
+        <li>
+            <div class="collapsible-header" @click="showLanguages"><i class="material-icons">language</i>
+                <p id="language">Language</p>
+            </div>
+            <div class="collapsible-body padding-1rem" style="padding: 1rem !important;">
+        <li v-for="(lang, index) in dict" :key="index">
+            <a href="#!" @click="showLanguageMenu(index)">{{ lang.name }}</a>
+        </li>
+        </div>
+        </li>
+    </ul>
 </template>
 
 <style>
 @import "./assets/base.css";
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
 
-  font-weight: normal;
-  width: 45%;
+    font-weight: normal;
+    width: 45%;
 }
 
 @media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
+    body {
+        display: flex;
+        place-items: center;
+    }
 }
 
 @media (max-width: 412px) {
-  #app {
-    width: 100vw;
-    min-height: 100vh;
-  }
+    #app {
+        width: 100vw;
+        min-height: 100vh;
+    }
 }
 
 .title {
-  color: black;
-  text-align: center
+    color: black;
+    text-align: center
 }
 
 .padding-bottom-16 {
-  padding-bottom: 16px;
+    padding-bottom: 16px;
 }
 
 .padding-1rem {
-  padding-right: 1rem !important;
-  padding-left: 1rem !important;
-  padding-top: 1rem !important;
-  padding-bottom: 1rem !important;
+    padding-right: 1rem !important;
+    padding-left: 1rem !important;
+    padding-top: 1rem !important;
+    padding-bottom: 1rem !important;
 }
 
 .dropdownWidth {
-  width: 192px !important;
+    width: 192px !important;
 }
 
 .box-gh-badge {
-  display: flex;
-  justify-content: center;
-  flex-direction: row;
-  flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
 }
+
 </style>
--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {reactive, watch} from 'vue';
+import { reactive, watch } from 'vue';
 import TimeInput from "./TimeInput.vue";
 import helpers from "../assets/js/helpers.js";
 import { toggleLanguage } from "../assets/js/lang.js";
@@ -12,99 +12,102 @@ timeValues.hours = JSON.parse(helpers.fetchValue("hours")) || [];
 timeValues.start = helpers.fetchValue("start-input") || "08:00";
 timeValues.lunch = helpers.fetchValue("lunch-input") || "11:00";
 timeValues.back = helpers.fetchValue("back-input") || "13:00";
-    // document.getElementById("notification").checked =
-        // fetchValue("notification") == "true" ? true : false;
-    // setHour();
-    // const statusDarkMode = fetchValue("darkmode") == "true" ? true : false;
-    // document.getElementById("darkmode").checked = statusDarkMode;
-    // toggleDarkMode(statusDarkMode);
+// document.getElementById("notification").checked =
+// fetchValue("notification") == "true" ? true : false;
+// setHour();
+// const statusDarkMode = fetchValue("darkmode") == "true" ? true : false;
+// document.getElementById("darkmode").checked = statusDarkMode;
+// toggleDarkMode(statusDarkMode);
 
 helpers.setHour(timeValues);
 
-setInterval(()=>{
-  timeValues.now = helpers.normalizeTime(new Date());
-  helpers.subtractTime(timeValues.now, timeValues.end);
+setInterval(() => {
+    timeValues.now = helpers.normalizeTime(new Date());
+    helpers.subtractTime(timeValues.now, timeValues.end);
 }, 1000);
 
 watch(timeValues, () => {
-  helpers.setHour(timeValues);
-  if (timeValues.left != '' || timeValues.overtime != '') {
-    setTimeout(() => {
-      toggleLanguage();
-    }, 0);
-  }
+    helpers.setHour(timeValues);
+    if (timeValues.left != '' || timeValues.overtime != '') {
+        setTimeout(() => {
+            toggleLanguage();
+        }, 0);
+    }
 });
 
-function addNewAdditionalHours(){
-  const time = {
-    start: '',
-    end: ''
-  };
+function addNewAdditionalHours() {
+    const time = {
+        start: '',
+        end: ''
+    };
 
-  if (timeValues.hours.length == 0) {
-    time.start = timeValues.back;
-    time.end = helpers.addTime(timeValues.back, '00:30');
-  } else {
-    time.start = helpers.addTime(timeValues.hours.at(-1).end, '00:30');
-    time.end = helpers.addTime(time.start, '00:30');
-  }
+    if (timeValues.hours.length == 0) {
+        time.start = timeValues.back;
+        time.end = helpers.addTime(timeValues.back, '00:30');
+    } else {
+        time.start = helpers.addTime(timeValues.hours.at(-1).end, '00:30');
+        time.end = helpers.addTime(time.start, '00:30');
+    }
 
-  timeValues.hours.push(time);
-  timeValues.back = helpers.addTime(time.end, '01:00');
+    timeValues.hours.push(time);
+    timeValues.back = helpers.addTime(time.end, '01:00');
 
-  setTimeout(() => {
-    toggleLanguage();
-  }, 0);
+    setTimeout(() => {
+        toggleLanguage();
+    }, 0);
 }
 
-function removeAdditionalHour(index){
-  timeValues.hours.splice(index, 1);
+function removeAdditionalHour(index) {
+    timeValues.hours.splice(index, 1);
 }
 
 
 </script>
 
 <template>
-  <div class="col s12">
-    <TimeInput id="dailyHours" for="daily-hours" label="Daily Hours:" v-model="timeValues.daily" left="left: 0px;" />
-    <div>
-      <div class="card row s12 m6" style="padding: 6px;">
-        <div class="col s10 m11" style="padding-left: 0px;">
-          <TimeInput id="starting" for="start-input" label="Start:" v-model="timeValues.start"
-            style="margin-bottom: 0px;" />
-          <TimeInput id="stoping" for="lunch-input" label="Lunch time:" v-model="timeValues.lunch"
-            style="margin-bottom: 0px;" />
+    <div class="col s12">
+        <TimeInput id="dailyHours" for="daily-hours" label="Daily Hours:" v-model="timeValues.daily" left="left: 0px;" />
+        <div>
+            <div class="card row s12 m6 rouded-box" style="padding: 6px;">
+                <div class="col s10 m11">
+                    <TimeInput id="starting" for="start-input" label="Start:" v-model="timeValues.start"
+                        style="margin-bottom: 0px;" />
+                    <TimeInput id="stoping" for="lunch-input" label="Lunch time:" v-model="timeValues.lunch"
+                        style="margin-bottom: 0px;" />
+                </div>
+                <div class="col s2 m1">
+                    <button class="right waves-effect waves-light rounded-button s2 m2"
+                        v-on:click="addNewAdditionalHours()"><i class="material-icons" style="top: 2px;">add</i></button>
+                </div>
+            </div>
+            <div class="card row s12 m6 rouded-box fadeIn" v-for="(hour, index) in timeValues.hours" :key="index"
+                style="padding: 6px;">
+                <div class="col s10 m11">
+                    <TimeInput id="starting" for="start-input" label="Start:" v-model="hour.start"
+                        style="margin-bottom: 0px;" />
+                    <TimeInput id="stoping" for="lunch-input" label="Lunch time:" v-model="hour.end"
+                        style="margin-bottom: 0px;" />
+                </div>
+                <div class="col s2 m1">
+                    <button class="right waves-effect waves-light rounded-button"
+                        v-on:click="removeAdditionalHour(index)"><i class="material-icons"
+                            style="top: 2px;">delete</i></button>
+                </div>
+            </div>
+            <div class="row s12 m6">
+                <TimeInput id="back" for="back-input" label="Back to work:" v-model="timeValues.back" />
+                <TimeInput id="ending" for="stop-input" label="Stop! 🙂:" v-model="timeValues.end" readonly="true" />
+            </div>
         </div>
-        <div class="col s2 m1">
-          <button class="right waves-effect waves-light rounded-button s2 m2" v-on:click="addNewAdditionalHours()"><i
-              class="material-icons" style="top: 2px;">add</i></button>
-        </div>
-      </div>
-      <div class="card row s12 m6" v-for="(hour,index) in timeValues.hours" :key="index" style="padding: 6px;">
-        <div class="col s10 m11" style="padding-left: 0px;">
-          <TimeInput id="starting" for="start-input" label="Start:" v-model="hour.start" style="margin-bottom: 0px;" />
-          <TimeInput id="stoping" for="lunch-input" label="Lunch time:" v-model="hour.end"
-            style="margin-bottom: 0px;" />
-        </div>
-        <div class="col s2 m1">
-          <button class="right waves-effect waves-light rounded-button" v-on:click="removeAdditionalHour(index)"><i
-              class="material-icons" style="top: 2px;">delete</i></button>
-        </div>
-      </div>
-      <div class="row s12 m6">
-        <TimeInput id="back" for="back-input" label="Back to work:" v-model="timeValues.back" />
-        <TimeInput id="ending" for="stop-input" label="Stop! 🙂:" v-model="timeValues.end" readonly="true" />
-      </div>
+        <TimeInput id="leftHours" for="left-hours" label="Hours left:" left="left: 0px;" v-model="timeValues.left"
+            readonly="true" v-if="timeValues.left != ''" />
+        <TimeInput id="overtime" for="overtime-input" label="Overtime:" left="left: 0px;" v-model="timeValues.overtime"
+            readonly="true" v-if="timeValues.overtime != ''" />
     </div>
-    <TimeInput id="leftHours" for="left-hours" label="Hours left:" left="left: 0px;" v-model="timeValues.left"
-      readonly="true" v-if="timeValues.left != ''" />
-    <TimeInput id="overtime" for="overtime-input" label="Overtime:" left="left: 0px;" v-model="timeValues.overtime"
-      readonly="true" v-if="timeValues.overtime != ''" />
-  </div>
 </template>
 
 <style scoped>
-  .rounded-button {
+.rounded-button {
     color: white;
     background-color: mediumpurple;
     margin: 15px 0px;
@@ -113,11 +116,69 @@ function removeAdditionalHour(index){
     font-size: 1.8rem;
     width: 40px;
     height: 40px;
-  }
+}
 
-  @media (max-width: 412px) {
-    .rounded-button {
-      bottom: -66px;
+.rouded-box {
+    border-radius: 12px;
+    -webkit-animation-duration: 0.5s;
+    animation-duration: 0.5s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+}
+
+@-webkit-keyframes fadeIn {
+    0% {
+        opacity: 0;
     }
-  }
+
+    100% {
+        opacity: 1;
+    }
+}
+
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes fadeOut {
+    0% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
+}
+
+@keyframes fadeOut {
+    0% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
+}
+
+.fadeIn {
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+}
+
+.fadeOut {
+    -webkit-animation-name: fadeOut;
+    animation-name: fadeOut;
+}
+
+@media (max-width: 412px) {
+    .rounded-button {
+        bottom: -66px;
+    }
+}
 </style>
